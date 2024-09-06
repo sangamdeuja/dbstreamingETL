d80c0e07079d9d37dd894ef7200c78d11762a793
sangamdeuja <sangamad1@gmail.com>
2024-09-06 09:46:31 +0300

    modified etl_init.sh

--- etl_init.sh
+++ etl_init.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-databricks notebooks run --notebook-path "/Repos/sangamdeuja/dbstreamingETL/mountcontainer.ipynb"
+pip install databricks-cli
 
 
+databricks configure --token <<-EOF
+$DATABRICKS_HOST
+$DATABRICKS_TOKEN
+EOF
+
+
+databricks runs submit --json '{
+  "tasks": [
+    {
+      "task_key": "notebook-task",
+      "notebook_task": {
+        "notebook_path": "/Repos/your-username/your-repo/notebooks/etl_pipeline.py"
+      },
+      "existing_cluster_id": "'"$DB_CLUSTER_ID"'"
+    }
+  ]
+}'
