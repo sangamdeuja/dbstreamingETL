560b87f6390b5275c5b6ab4281d4cd33f52ce7a1
sangamdeuja <sangamad1@gmail.com>
2024-09-06 09:12:00 +0300

    modified etl_init.sh script

--- etl_init.sh
+++ etl_init.sh
@@ -2,5 +2,4 @@
 
 databricks notebooks run --notebook-path "/Repos/sangamdeuja/dbstreamingETL/mountcontainer.ipynb"
 
-# Step 3: Use Databricks CLI to run the etlpipeline.ipynb notebook
-databricks notebooks run --notebook-path "/Repos/sangamdeuja/dbstreamingETL/deltapipeline.ipynb"
+
