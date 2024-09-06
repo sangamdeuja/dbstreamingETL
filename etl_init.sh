a1542a05d596259e4d1833baeaef8ada49d68d27
sangamdeuja <sangamad1@gmail.com>
2024-09-06 09:00:21 +0300

    corrected repos filepath

--- etl_init.sh
+++ etl_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-databricks notebooks run --notebook-path "/Repos/sangamdeuja/repo/mountcontainer.ipynb"
+databricks notebooks run --notebook-path "/Repos/sangamdeuja/dbstreamingETL/mountcontainer.ipynb"
 
 # Step 3: Use Databricks CLI to run the etlpipeline.ipynb notebook
-databricks notebooks run --notebook-path "/Repos/sangamdeuja/repo/deltapipeline.ipynb"
+databricks notebooks run --notebook-path "/Repos/sangamdeuja/dbstreamingETL/deltapipeline.ipynb"
