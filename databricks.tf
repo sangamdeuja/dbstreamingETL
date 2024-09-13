4cc95b60afe7049596becf0f90fa7265f0efb693
sangamdeuja <sangamad1@gmail.com>
2024-09-13 22:14:02 +0300

    modified path in databricks_notebook resource

--- databricks.tf
+++ databricks.tf
@@ -52,7 +52,7 @@ data "databricks_spark_version" "latest_lts" {
 
 resource "databricks_notebook" "notebook_1" {
   source = "${path.module}/pipeline_scripts/mountcontainer.ipynb"
-  path   = "${data.databricks_current_user.me.home}/mountnotebook/mountcontainer"
+  path   = "${data.databricks_current_user.me.home}/mountnotebook"
 
 }
 
